05198143b1af86185100fd2f54ab7044d9bd4649
Richard Dinh <dick.dinh@gmail.com>
2014-06-16 17:05:34 -0400

    make javascript example work.

--- js-example/js-example.php
+++ js-example/js-example.php
@@ -7,3 +7,15 @@ Version: 1.0
 Author: Richard Dinh
 License: GPLv2
 */
+
+/*
+ *  Load styles and scripts 
+ * see reference:
+ * http://codex.wordpress.org/Function_Reference/wp_enqueue_script
+ */
+function js_example_enqueue_scripts(){  
+  // this will add the js on any single page.
+  wp_enqueue_script( 'script-name', plugins_url('js-example') . '/js/js-example.js', array(), '1.0.0', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'js_example_enqueue_scripts' );
